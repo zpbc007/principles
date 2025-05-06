16013d1e2990d85d146c62f4556cc43dfb1809a6
zpbc007 <719197352@qq.com>
2025-05-06 15:18:41 +0800

    fix: nav link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,23 +1,21 @@
 export default {
-  title: '方法论',
-  description: '大霸哥收集的各种方法论',
+  title: "方法论",
+  description: "大霸哥收集的各种方法论",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: '时间管理', link: '/time_management/' },
+      { text: "Home", link: "/" },
+      { text: "时间管理", link: "/time_management/four_quadrant/" },
     ],
     sidebar: {
-      '/time_management/': [
+      "/time_management/": [
         {
-          text: '时间管理',
-          items: [
-            { text: '四象限', link: '/time_management/four_quadrant/' },
-          ]
-        }
-      ]
+          text: "时间管理",
+          items: [{ text: "四象限", link: "/time_management/four_quadrant/" }],
+        },
+      ],
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/zpbc007/principles-valut' }
-    ]
-  }
-}
+      { icon: "github", link: "https://github.com/zpbc007/principles-valut" },
+    ],
+  },
+};
